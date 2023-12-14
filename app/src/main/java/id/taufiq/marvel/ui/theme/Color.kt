2a474290c1833f4638cc1aa36a5c9db611fc0664
mtaufiqh <mtaufiqh62@gmail.com>
2023-12-14 16:33:04 +0800

    update gradle version and compose version

--- app/src/main/java/id/taufiq/marvel/ui/theme/Color.kt
+++ app/src/main/java/id/taufiq/marvel/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package id.taufiq.marvel.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFFBD0D21)
+val md_theme_light_onPrimaryContainer = Color(0xFF410004)
+val md_theme_light_secondary = Color(0xFF984061)
+val background = Color(0xFF000000)
